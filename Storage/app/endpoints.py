18b84d98a1ed5dc62bec5e6049f4267cc346833e
baiyuchen1228 <baiyuchen1228@gmail.com>
2023-02-10 17:41:27 +0800

    aaa
    aaa

--- Storage/app/endpoints.py
+++ Storage/app/endpoints.py
@@ -10,6 +10,8 @@ from app.settings import get_settings
 from app.settings import Settings
 from fastapi import FastAPI
 
+import json
+
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -22,19 +24,32 @@ CACHE: dict[
 ] = defaultdict(lambda: defaultdict(list))
 
 
+
 @app.post('/records')
 def save(record: Record) -> Result:
     location_dict = CACHE[record.location]
     records = location_dict[record.timestamp[0:10]]  # datetime to date
     records.append(record)
+    with open(r'./data.json', 'a') as f:
+        json_data = json.dumps(record.dict())
+        f.write(json_data))
     return Result.ok()
 
 
 @app.get('/records')
 def query(location: str, date: str) -> list[Record]:
-    if location_dict := CACHE.get(location):
-        return location_dict.get(date)
-    return []
+    # if location_dict := CACHE.get(location):
+    #     return location_dict.get(date)
+    with open('./data.json') as f:
+        # data = json.load(f)
+        # new_data = pd.DataFrame()
+        # new_data = data.iloc[[0,2]]
+        # return new_data
+        input_dict = json.load(f)
+        print(input_dict)
+        output_dict = [x for x in input_dict if 'location' in x and x['location'] == location]
+        output_json = json.dumps(output_dict)
+    return output_json
 
 
 @app.get('/report')
